9577ab36b4832d320dd613969d542de9af5dfd03
wichian ph. <ph.wichian@gmail.com>
2024-10-30 17:14:58 +0700

    Add files via upload

---
+++ Addition/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1596172056,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/number-sheet0.png",
+		"images/number-sheet1.png",
+		"images/number-sheet2.png",
+		"images/plus-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/box-sheet0.png",
+		"images/base-sheet0.png",
+		"images/shadow-sheet0.png",
+		"images/box2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ clock/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1599473602,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/face-sheet0.png",
+		"images/hourleg-sheet0.png",
+		"images/minleg-sheet0.png",
+		"images/box-sheet0.png",
+		"images/boxdrop-sheet0.png",
+		"images/symbol-sheet0.png",
+		"images/symbol-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/shadow-sheet0.png",
+		"images/shadow-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
